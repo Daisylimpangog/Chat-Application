f112b76d714172cf3ee2e4343bc1f3b94f66c519
Daisylimpangog <119760453+Daisylimpangog@users.noreply.github.com>
2023-10-07 17:27:04 +0800

    commit 3

--- ChatApp/src/views/LoginView.vue
+++ ChatApp/src/views/LoginView.vue
@@ -1,38 +1,137 @@
 <template>
-    <div>
-    <h1>Login</h1>
-    <form>
-        <label for="email"> Email</label>
-        <input type="email" name="email" id="email">
-        <br>
-        <label for="password"> Password </label>&nbsp
-        <input type="password" name="password" id="password">
-        <br>
-        <button type="submit">Login</button>
-    </form>
-</div>
+  <form>
+    <div class="container">
+      <h1>LOG IN!</h1>
+      <p>Fill the form below to <strong>Log In</strong></p>
+      <hr style="border: 1px solid #999" />
+
+      <div class="form-group">
+        <input
+          class="form-control"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Your Email *"
+          autocomplete="off"
+          spellcheck="false"
+          required
+        />
+        <label for="email">Email Address</label>
+      </div>
+
+      <div class="form-group">
+        <input
+          class="form-control"
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter Your Password *"
+          autocomplete="off"
+          spellcheck="false"
+          required
+        />
+        <label for="password">Password</label>
+      </div>
+
+      <hr style="border: 1px solid #999; margin-top: 0.8rem" />
+      <input class="btn" type="submit" name="submit" value="LOG IN" />
+    </div>
+  </form>
 </template>
 
 <style scope>
-div
-{
-    background: #474bff;
-    background: -webkit-linear-gradient(0deg, #474bff 0%, #bc48ff 100%);
-    background: linear-gradient(0deg, #474bff 0%, #bc48ff 100%);
-    -webkit-box-shadow: -1px -6px 12px 0.6px gray;
-     -moz-box-shadow: -1px -6px 12px -0.5px #dddddd;
-    box-shadow: -1px -6px 12px -0.5px white;
-    
-
+body {
+  background: #9b9ce0;
+  background: -webkit-radial-gradient(circle, #9b9ce0 0%, #8c2cc4 100%);
+  background: radial-gradient(circle, #9b9ce0 0%, #8c2cc4 100%);
 }
-form {
-    
-    background: rgba(255,255,255,0.35);
--webkit-backdrop-filter: blur(10px);
-backdrop-filter: blur(10px);
-border: 1px solid rgba(255,255,255,0.175);
-
+/* Apply a reset to remove default browser styles */
+body,
+h1,
+p,
+form,
+input,
+hr {
+  margin: 0;
+  padding: 0;
 }
 
+/* Apply styles to the container */
+.container {
+  max-width: 460px;
+  margin: 0 auto;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.55);
+  border: 1px solid rgba(255, 255, 255, 0.275);
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+}
 
+/* Style the header */
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+  padding: 1px;
+  border: 1px;
+}
+
+/* Style the paragraph */
+p {
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
+/* Style the form-group */
+.form-group {
+  margin-bottom: 20px;
+  width: 90%;
+}
+
+/* Style the input fields */
+.form-control {
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  font-size: 16px;
+}
+
+/* Style the labels */
+label {
+  display: block;
+  font-weight: bold;
+}
+
+/* Style the horizontal rule */
+hr {
+  border: 1px solid #999;
+  margin-top: 0.8rem;
+  margin-bottom: 20px;
+}
+
+/* Style the submit button */
+.btn {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 3px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.btn:hover {
+  background-color: #0056b3;
+}
+
+/* Apply styles to form elements on focus */
+.form-control:focus {
+  outline: none;
+  border-color: #007bff;
+  box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+}
 </style>
